5a9528363655e279abdee2b720e6ffda60474fd0
Allen Suárez <48367124+hub420@users.noreply.github.com>
2023-04-13 08:37:04 -0600

    Update 8_pro.py
    add the line 5 the false output

--- environment/8_pro.py
+++ environment/8_pro.py
@@ -2,7 +2,7 @@ print("Welcome to Guess the Number!")
 print("The rules are simple. I will think of a number, and you will try to guess it.")
 import random
 number = random.randint(1,10)
-
+isGuessRight = False
 while isGuessRight != True:
     guess = input("Guess a number between 1 and 10: ")
     if int(guess) == number:
